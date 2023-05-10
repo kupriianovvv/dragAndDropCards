f011dda2b1a1316390106e3e47afb8fc6ae53bd8
kupriianovvv <kupriianovvv@gmail.com>
2023-05-10 09:35:56 +0300

    adds click tracking in useMove hooks

--- src/hooks/useMove.jsx
+++ src/hooks/useMove.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef } from "react";
 
 export const useMove = (id) => {
+  const isClicked = useRef(false);
+
   const coords = useRef({
     startX: 0,
     startY: 0,
@@ -13,16 +15,20 @@ export const useMove = (id) => {
     const whiteboard = card.parentElement;
 
     const onMouseDown = (e) => {
+      isClicked.current = true;
       coords.current.startX = e.clientX;
       coords.current.startY = e.clientY;
     };
 
     const onMouseUp = (e) => {
+      isClicked.current = false;
       coords.current.endX = card.offsetLeft;
       coords.current.endY = card.offsetTop;
     };
 
     const onMouseMove = (e) => {
+      if (!isClicked.current) return;
+
       const nextX = e.clientX - coords.current.startX + coords.current.endX;
       const nextY = e.clientY - coords.current.startY + coords.current.endY;
 
