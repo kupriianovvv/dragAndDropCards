dc74873ab56d8d36d2bfcea6a9b9e25a066f3fc5
kupriianovvv <kupriianovvv@gmail.com>
2023-06-17 10:12:19 +0300

    cleans useCanvasPosition

--- src/hooks/useCanvasPosition.jsx
+++ src/hooks/useCanvasPosition.jsx
@@ -4,49 +4,6 @@ export const useCanvasPosition = (initialCanvasPosition) => {
   const [canvasPosition, setCanvasPosition] = useState(initialCanvasPosition);
 
   useEffect(() => {
-    const getNewCanvasPositionFromPrevWhenScrollAndCTRLOrCommand = (e) => {
-      return (prevCanvasPosition) => {
-        const normedCoords = {
-          x: (e.clientX - prevCanvasPosition.x) / prevCanvasPosition.scale,
-          y: (e.clientY - prevCanvasPosition.y) / prevCanvasPosition.scale,
-        };
-
-        const newScale =
-          e.wheelDelta > 0
-            ? prevCanvasPosition.scale * 1.1
-            : prevCanvasPosition.scale / 1.1;
-        const newCoords = {
-          x: e.clientX - normedCoords.x * newScale,
-          y: e.clientY - normedCoords.y * newScale,
-        };
-        return {
-          x: newCoords.x,
-          y: newCoords.y,
-          scale: newScale,
-        };
-      };
-    };
-
-    const getNewCanvasPositionFromPrevWhenScrollAndShift = (e) => {
-      return (prevCanvasPosition) => {
-        return {
-          x: prevCanvasPosition.x + 0.5 * e.wheelDelta,
-          y: prevCanvasPosition.y,
-          scale: prevCanvasPosition.scale,
-        };
-      };
-    };
-
-    const getNewCanvasPositionFromPrevWhenScroll = (e) => {
-      return (prevCanvasPosition) => {
-        return {
-          x: prevCanvasPosition.x,
-          y: prevCanvasPosition.y + 0.5 * e.wheelDelta,
-          scale: prevCanvasPosition.scale,
-        };
-      };
-    };
-
     const createGetNewCanvasPositionFromPrev = (e) => {
       function getNewCanvasPositionFromPrevWhenScrollAndCTRLOrCommand(
         prevCanvasPosition
